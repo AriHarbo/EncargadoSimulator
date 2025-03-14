b481e5f4ef796367d797056860b42f2c02d8a82e
AriHarbo <arielharbo.219@gmail.com>
2025-03-14 16:45:56 -0300

    fixed trash script

--- src/scripts/mapa.gd
+++ src/scripts/mapa.gd
@@ -1,14 +1,9 @@
 extends Node3D
 
-var trash_can_scene = preload("res://src/scenes/trash_can.tscn")
+
 var trash_scene = preload("res://src/scenes/trash.tscn")
 
 func _ready():
-	# Instanciar y colocar el tacho de basura
-	var trash_can_instance = trash_can_scene.instantiate()
-	add_child(trash_can_instance)
-	trash_can_instance.position = Vector3(10,1.5, 0)
-
 	for i in range(5):  # 5 basuras como ejemplo
 		var trash_instance = trash_scene.instantiate()
 		add_child(trash_instance)
